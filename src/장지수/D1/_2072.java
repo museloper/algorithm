3776538c1e97b24ae2cd93d5f98ae1f53b51cc29
museloper <nosf6842@naver.com>
2020-12-30 16:00:25 +0900

    <장지수> 2072. 홀수만 더하기

---
+++ src/장지수/D1/_2072.java
@@ -0,0 +1,35 @@
+package 장지수.D1;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class _2072 {
+	
+	private static Scanner scanner;
+
+	public static void main(String[] args) throws Exception {
+		
+		System.setIn(new FileInputStream("input/_2072.txt"));
+
+		scanner = new Scanner(System.in);
+		
+		int tc = Integer.parseInt(scanner.nextLine());
+		
+		for(int i=0; i<tc; i++) {
+			String arr[] = scanner.nextLine().split(" ");
+
+			int sum = 0;
+			
+			for(int j=0; j<arr.length; j++) {
+				int num = Integer.parseInt(arr[j]);
+				if(num % 2 != 0) sum += num;
+			}
+			
+			System.out.println("#" + (i+1) + " " + sum);
+		}
+		
+		scanner.close();
+		
+		// RUNTIME : 0.12937s
+	}
+}
