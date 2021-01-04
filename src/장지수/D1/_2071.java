b57bf922ce2418fda3f5a899d457523a5af1a937
museloper <nosf6842@naver.com>
2021-01-04 15:45:24 +0900

    <장지수> 2071. 평균값 구하기

---
+++ src/장지수/D1/_2071.java
@@ -0,0 +1,36 @@
+package 장지수.D1;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class _2071 {
+
+	private static Scanner scanner;
+
+	public static void main(String[] args) throws Exception {
+		
+		System.setIn(new FileInputStream("input/_2071.txt"));
+
+		scanner = new Scanner(System.in);
+		
+		int testCase = Integer.parseInt(scanner.nextLine());
+		
+		for(int i=0; i<testCase; i++) {
+			String arr[] = scanner.nextLine().split(" ");
+
+			long sum = 0;
+			
+			for(String num : arr) {
+				sum += Integer.parseInt(num);
+			}
+			
+			System.out.println("#"+(i+1) + " " + Math.round((double)sum/arr.length));
+		}
+		
+		scanner.close();
+		
+		// RUNTIME : 0.12953s
+
+	}
+
+}
