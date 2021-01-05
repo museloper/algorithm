85cc400a8b836410cbad1c7e91da00a9d16b58ef
museloper <nosf6842@naver.com>
2021-01-05 17:43:39 +0900

    <장지수> 2070. 큰 놈, 작은 놈, 같은 놈

---
+++ src/장지수/D1/_2070.java
@@ -0,0 +1,38 @@
+package 장지수.D1;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class _2070 {
+
+	private static Scanner scanner;
+
+	public static void main(String[] args) throws Exception {
+
+		System.setIn(new FileInputStream("input/_2070.txt"));
+
+		scanner = new Scanner(System.in);
+		
+		int testCase = Integer.parseInt(scanner.nextLine());
+		
+		for(int i=0; i<testCase; i++) {
+			String arr[] = scanner.nextLine().split(" ");
+			
+			int num1 = Integer.parseInt(arr[0]);
+			int num2 = Integer.parseInt(arr[1]);
+			
+			if(num1 > num2) {
+				System.out.println("#"+(i+1) + " " + ">");
+			} else if(num1 == num2) {
+				System.out.println("#"+(i+1) + " " + "=");
+			} else {
+				System.out.println("#"+(i+1) + " " + "<");
+			}
+		}
+
+		scanner.close();
+		
+		// RUNTIME : 0.12250s
+	}
+
+}
