f9d97dee1791cb32fad23dab5bce4ae47cf8fa46
museloper <nosf6842@naver.com>
2021-01-06 16:06:05 +0900

    <장지수> 2068. 최대수 구하기

---
+++ src/장지수/D1/_2068.java
@@ -0,0 +1,35 @@
+package 장지수.D1;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class _2068 {
+
+	private static Scanner scanner;
+
+	public static void main(String[] args) throws Exception {
+
+		System.setIn(new FileInputStream("input/_2068.txt"));
+
+		scanner = new Scanner(System.in);
+
+		int testCase = Integer.parseInt(scanner.nextLine().trim());
+		
+		for(int i=0; i<testCase; i++) {
+			String arr[] = scanner.nextLine().split(" ");
+			
+			int max = 0;
+			
+			for(String num : arr) {
+				if(max < Integer.parseInt(num)) {
+					max = Integer.parseInt(num);
+				}
+			}
+			
+			System.out.println("#"+(i+1) + " " + max);
+			
+			// RUNTIME : 0.12385s
+		}
+	}
+
+}
