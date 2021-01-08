b28fdf296fd54de5c86dcf89f1c70177c3f533a3
mococomo90 <gmsp2002@fusionsoft.co.kr>
2021-01-08 11:54:36 +0900

    <이진화> 2072 문제

--- src/ljh/D1/_2072.java
+++ src/ljh/D1/_2072.java
@@ -1,9 +1,44 @@
 package ljh.D1;
 
+import java.util.Scanner;
+
 public class _2072 {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int lineCnt = Integer.parseInt(sc.nextLine());
+		
+		int totalCnt = 0;
+		
+		String arr[];
+		
+		System.out.println();
+		System.out.println();
+		
+		for(int i = 0; i < lineCnt; i++) {
+
+			arr = sc.nextLine().split(" ");
+			
+			for(int j = 0; j < arr.length; j++) {
+				
+				int arrNum = Integer.parseInt(arr[j]);
+				
+				int mod = arrNum % 2;
+				
+				if (mod > 0) {
+					totalCnt += arrNum;
+				}
+				
+			}
+			
+			System.out.println("#" + (i+1) + " " + totalCnt);
+			
+			totalCnt = 0;
+			
+		}
 	}
 
 }
