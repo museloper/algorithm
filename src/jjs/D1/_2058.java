c0f7af3962971ce97efe3b502d93a7ecbf2d8dc4
museloper <nosf6842@naver.com>
2021-01-08 09:19:09 +0900

    <장지수> 2058. 자릿수 더하기

---
+++ src/jjs/D1/_2058.java
@@ -0,0 +1,30 @@
+package jjs.D1;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class _2058 {
+	
+	private static Scanner scanner;
+
+	public static void main(String[] args) throws Exception {
+
+		System.setIn(new FileInputStream("input/_2058.txt"));
+
+		scanner = new Scanner(System.in);
+		
+		int num = Integer.parseInt(scanner.nextLine().trim());
+		
+		int sum = 0;
+		
+		while(num  > 0) {
+			sum += num % 10;
+			num /= 10;
+		}
+		
+		System.out.println(sum);
+		
+		// RUNTIME : 0.12903s
+	}
+
+}
