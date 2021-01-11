9482519d858b5fb46f1393a535f47245acab1589
museloper <nosf6842@naver.com>
2021-01-11 09:13:12 +0900

    <장지수> 2050. 알파벳을 숫자로 변환

---
+++ src/jjs/D1/_2050.java
@@ -0,0 +1,27 @@
+package jjs.D1;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class _2050 {
+
+	private static Scanner scanner;
+	
+	public static void main(String[] args) throws Exception {
+
+		System.setIn(new FileInputStream("input/_2050.txt"));
+
+		scanner = new Scanner(System.in);
+		
+		String str = scanner.nextLine();
+		
+		char[] varchar = str.toCharArray();
+		
+		for(char convert : varchar) {
+			System.out.print(convert-64 + " ");
+		}
+		
+		// RUNTIME : 0.12832s
+	}
+
+}
