49485cd7c995eb92f6193507fadd5c3608512465
museloper <nosf6842@naver.com>
2021-01-13 09:17:26 +0900

    <장지수> 2043. 서랍의 비밀번호

---
+++ src/jjs/D1/_2043.java
@@ -0,0 +1,36 @@
+package jjs.D1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class _2043 {
+	
+	private static Scanner scanner;
+
+	public static void main(String[] args) throws Exception {
+		
+		System.setIn(new FileInputStream("input/_2043.txt"));
+		
+		scanner = new Scanner(System.in);
+		
+		String[] number = scanner.nextLine().split(" ");
+		
+		int password = Integer.parseInt(number[0]);
+		int startNum = Integer.parseInt(number[1]);
+		
+		int cnt = 0;
+		
+		for(int i=startNum; i<=password; i++) {
+			if(startNum == password) {
+				break;
+			}
+			++cnt;
+		}
+		
+		System.out.print(cnt);
+		
+		// RUNTIME : 0.12797s
+	}
+
+}
---
+++ src/jjs/D1/_2046.java
@@ -0,0 +1,26 @@
+package jjs.D1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class _2046 {
+
+	private static Scanner scanner;
+	
+	public static void main(String[] args) throws Exception {
+		
+		System.setIn(new FileInputStream("input/_2046.txt"));
+		
+		scanner = new Scanner(System.in);
+		
+		int cnt = Integer.parseInt(scanner.nextLine());
+		
+		for(int i=0; i<cnt; i++) {
+			System.out.print("#");
+		}
+		
+		// RUNTIME : 0.13162s
+	}
+
+}
