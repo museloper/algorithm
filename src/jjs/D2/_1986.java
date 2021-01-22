2c9e6e9c43bbe9f032f9c24cebda27318f686075
museloper <nosf6842@naver.com>
2021-01-22 09:06:08 +0900

    <장지수> 1986. 지그재그 숫자

---
+++ src/jjs/D2/_1986.java
@@ -0,0 +1,36 @@
+package jjs.D2;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class _1986 {
+	
+	private static Scanner scanner;
+
+	public static void main(String[] args) throws Exception {
+
+		System.setIn(new FileInputStream("input/_1986.txt"));
+		
+		scanner = new Scanner(System.in);
+		
+		int testCase = Integer.parseInt(scanner.nextLine().trim());
+		
+		for(int i=0; i<testCase; i++) {
+			
+			int nextInt = Integer.parseInt(scanner.nextLine().trim());
+			int sum = 0;
+
+			for(int j=1; j<nextInt+1; j++) {
+				if(j%2 == 0) {
+					sum += -j;
+				} else {
+					sum += j;
+				}
+			}
+			System.out.println("#"+(i+1) + " " + sum);
+		}
+		
+		// RUNTIME : 0.12407s
+	}
+
+}
