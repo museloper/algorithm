c5e0c3078fe49bf2cbc9c913d5d56fc7c1d10cfb
museloper <nosf6842@naver.com>
2021-02-02 11:10:29 +0900

    <장지수> 1928. Base64 Decoder

---
+++ src/jjs/D2/_1928.java
@@ -0,0 +1,37 @@
+package jjs.D2;
+
+import java.io.FileInputStream;
+import java.util.Base64;
+import java.util.Base64.Decoder;
+import java.util.Scanner;
+
+public class _1928 {
+
+	private static Scanner scanner;
+	
+	public static void main(String[] args) throws Exception {
+
+		System.setIn(new FileInputStream("input/_1928.txt"));
+		
+		scanner = new Scanner(System.in);
+		
+		int testCase = Integer.parseInt(scanner.nextLine().trim());
+		
+		for(int i=0; i<testCase; i++) {
+			String str = scanner.nextLine().trim();
+
+			Decoder decoder = Base64.getDecoder();
+			
+			byte[] ascii = decoder.decode(str);
+			
+			System.out.print("#"+(i+1) + " ");
+			for(byte output : ascii) {
+				System.out.print((char)output);
+			}
+			System.out.println();
+		}
+		
+		// RUNTIME : 0.13073s
+	}
+
+}
